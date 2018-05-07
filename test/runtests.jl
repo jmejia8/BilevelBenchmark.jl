bb5ee1aae4793c3c50b43834ef895ef5b9e0ffd9
Jesus Mejia <jesusmejded@gmail.com>
2018-05-06 23:39:46 -0500

    some test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,5 @@ using BilevelBenchmark
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@test bilevel_leader(ones(1000), ones(1000), 1) ≈ bilevel_leader(ones(1000), ones(1000), 1)
+@test bilevel_leader(ones(1000), ones(1000), 2) ≈ 0
