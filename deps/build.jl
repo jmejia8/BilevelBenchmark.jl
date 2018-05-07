8e55fee6ac4c8b9d8cbbfc12f2197bdb290864b8
Jesus Mejia <jesusmejded@gmail.com>
2018-05-06 23:10:19 -0500

    builder

---
+++ deps/build.jl
@@ -0,0 +1,32 @@
+using BinDeps
+using Compat.Libdl
+
+@BinDeps.setup
+
+bilevelBenchmark = library_dependency("bilevelBenchmarkJulia", aliases=["blb18_op"], os = :Unix)
+
+# build from source
+provides(Sources,
+        URI("https://github.com/jmejia8/bilevel-benchmark/archive/master.zip"),
+        unpacked_dir="bilevel-benchmark-master",
+        bilevelBenchmark)
+
+# provides(BuildProcess, Autotools(libtarget = "libgsl.la"), bilevelBenchmark)
+
+prefix = joinpath(BinDeps.depsdir(bilevelBenchmark), "usr")
+srcdir = joinpath(BinDeps.srcdir(bilevelBenchmark), "bilevel-benchmark-master")
+
+println(prefix)
+
+provides(SimpleBuild,
+    (@build_steps begin
+        GetSources(bilevelBenchmark)
+        CreateDirectory(joinpath(prefix, "lib"))
+        @build_steps begin
+            ChangeDirectory(srcdir)
+            MAKE_CMD
+            `mv blb18_op.$(Libdl.dlext) "$prefix/lib"`
+        end
+    end), [bilevelBenchmark], os = :Unix)
+
+@BinDeps.install Dict(:bilevelBenchmarkJulia => :bilevelBenchmark)
