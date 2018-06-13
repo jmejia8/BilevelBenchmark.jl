15a3891f7e8c545dca86919c4fb50ee3bcadd1e1
Jesús Mejía <jesusmejded@gmail.com>
2018-06-12 22:51:51 -0500

    new test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,16 @@ using Base.Test
 
 # write your own tests here
 
-@testset "SDM" begin
+@testset "bilevel" begin
     for i = 1:8
         @test bilevel_leader(ones(1000), ones(1000), i) ≈ bilevel_leader(ones(1000), ones(1000), i)
+    end
+end
+
+@testset "SDM" begin
+    p, q, r, s = 3, 3, 2, 0
+    for i = 1:8
+        @test SDM_leader(ones(1000), ones(1000), i, p, q, r, s) ≈ SDM_leader(ones(1000), ones(1000), i, p, q, r, s)
+        @test SDM_follower(ones(1000), ones(1000), i, p, q, r, s) ≈ SDM_follower(ones(1000), ones(1000), i, p, q, r, s)
     end
 end
