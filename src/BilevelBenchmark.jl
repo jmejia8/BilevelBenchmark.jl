d91e3c8d0fd593f91b91a7aabb7b566ae1fed849
Jesús Mejía <jesusmejded@gmail.com>
2018-08-27 15:11:58 -0500

    New features

--- src/BilevelBenchmark.jl
+++ src/BilevelBenchmark.jl
@@ -10,42 +10,118 @@ else
     error("This test function suit is not properly installed. Please run Pkg.build(\"BilevelBenchmark\")")
 end
 
-export bilevel_leader, bilevel_follower, SMD_leader, SMD_follower
+export bilevel_leader, bilevel_follower, SMD_leader, SMD_follower, bilevel_solutions
+export bilevel_settings, bilevel_ranges
+
+function bilevel_settings(D_ul::Int, D_ll::Int, fnum::Int)
+    r = div(D_ul, 2);
+    p = D_ul - r;
+    q = s = 0
+    
+    if fnum == 6
+        q = floor(Int, (D_ll - r) / 2);
+        s =  ceil(Int, (D_ll - r) / 2);
+    else
+        q = D_ll - r;
+    end
+
+    if fnum == 9
+        lenG = 1
+        leng = 1
+    elseif fnum == 10
+        lenG = p+r
+        leng = q
+    elseif fnum == 11
+        lenG = r
+        leng = 1
+    elseif fnum == 12
+        lenG = 2*r + p
+        leng = q+1
+    else
+        lenG = 0
+        leng = 0
+    end
+
+    return lenG, leng
+end
+
+function bilevel_solutions(D_ul::Int, D_ll::Int, fnum::Int)
+    x = zeros(D_ul)
+    y = zeros(D_ll)
+
+    ccall((:blb18_cop_solutions, bilevelBenchmark),
+              Void,
+            (Int32, Int32, Ptr{Cdouble}, Ptr{Cdouble}, Int32),
+            D_ul, D_ll, x, y, fnum)
+    
+    return x, y
+end
+
+function bilevel_ranges(D_ul::Int, D_ll::Int, fnum::Int)
+    bounds_ul = zeros(2D_ul)
+    bounds_ll = zeros(2D_ll)
+
+    ccall((:blb18_cop_ranges, bilevelBenchmark),
+              Void,
+            (Int32, Int32, Ptr{Cdouble}, Ptr{Cdouble}, Int32),
+            D_ul, D_ll, bounds_ul, bounds_ll, fnum)
+    
+    return reshape(bounds_ul, D_ul, :)', reshape(bounds_ll, D_ll, :)'
+end
 
 function bilevel_leader(x::Array{Float64}, y::Array{Float64}, fnum::Int)
-	D_upper = length(x)
-	D_lower = length(y)
-	F = [0.0]
+	D_ul = length(x)
+	D_ll = length(y)
+    F = [0.0]
+	G = [0.0]
+    lenG = 0
 
-	ccall((:blb18_leader_cop, bilevelBenchmark),
-		  Void,
-		(Int32, Int32, Int32, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Int32),
-		1, D_upper, D_lower, x, y, F, fnum)
-	
-	return F[1]
+    if fnum > 8
+        lenG, _ = bilevel_settings(D_ul, D_ll, fnum)
+        G = zeros(lenG)
+    end
+
+    ccall((:blb18_leader_cop, bilevelBenchmark),
+          Void,
+        (Int32, Int32, Int32, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Int32),
+        1, D_ul, D_ll, x, y, F, G, fnum)
+
+    if lenG == 0
+    	return F[1]
+    end
+
+    return F[1], G
+
+
+    
 end
 
 function bilevel_follower(x::Array{Float64}, y::Array{Float64}, fnum::Int)
-	D_upper = length(x)
-	D_lower = length(y)
-	F = [0.0]
+	D_ul = length(x)
+	D_ll = length(y)
+    f = [0.0]
+	g = [0.0]
+    leng = 0
+
+    if fnum > 8
+        _, leng = bilevel_settings(D_ul, D_ll, fnum)
+        g = zeros(leng)
+    end
 
 	ccall((:blb18_follower_cop, bilevelBenchmark),
 		  Void,
-		(Int32, Int32, Int32, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Int32),
-		1, D_upper, D_lower, x, y, F, fnum)
+		(Int32, Int32, Int32, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}, Int32),
+		1, D_ul, D_ll, x, y, f, g, fnum)
 	
-	return F[1]
+    if leng == 0
+        return f[1]
+    end
+
+    return f[1], g
 end
 
 
-function SMD_leader(x::Array{Float64},
-					  y::Array{Float64},
-				   fnum::Int,
-				      p::Int,
-				      q::Int,
-				      r::Int,
-				      s::Int = 0)
+function SMD_leader(x::Array{Float64}, y::Array{Float64}, fnum::Int,p::Int,q::Int,r::Int,s::Int = 0)
 	F = [0.0]
 
 	if fnum == 1
@@ -96,15 +172,9 @@ function SMD_leader(x::Array{Float64},
 end
 
 
-function SMD_follower(x::Array{Float64},
-					  y::Array{Float64},
-				   fnum::Int,
-				      p::Int,
-				      q::Int,
-				      r::Int,
-				      s::Int = 0)
-	D_upper = length(x)
-	D_lower = length(y)
+function SMD_follower(x::Array{Float64},y::Array{Float64},fnum::Int,p::Int,q::Int,r::Int,s::Int = 0)
+	D_ul = length(x)
+	D_ll = length(y)
 	F = [0.0]
 
 	if fnum == 1
