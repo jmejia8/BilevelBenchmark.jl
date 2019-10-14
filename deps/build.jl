3c6270f320d2bf922f64ee689f7ca69c38cce82d
Jesus Mejia <jesusmejded@gmail.com>
2019-10-14 11:46:45 -0500

    deps updated

--- deps/build.jl
+++ deps/build.jl
@@ -12,7 +12,7 @@ using Compat.Libdl
 
 @BinDeps.setup
 
-version = "9.1.2"
+version = "9.1.3"
 bilevelBenchmark = library_dependency("bilevelBenchmarkJulia", aliases=["blb18_op_v$version"], os = :Unix)
 
 # build from source
