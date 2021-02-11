9a2ddded93612ead7f0bdf38b97d2cda411cea1f
Jesús Mejía <jesusmejded@gmail.com>
2021-02-11 00:41:44 -0600

    Update BilevelBenchmark.jl

--- src/BilevelBenchmark.jl
+++ src/BilevelBenchmark.jl
@@ -15,7 +15,7 @@ export bilevel_leader, bilevel_follower, bilevel_solutions
 export bilevel_settings, bilevel_ranges
 export TP_settings,TP_leader,TP_follower,TP_test
 export SMD_settings, SMD_solutions, SMD_ranges, SMD_leader, SMD_follower
-export PMM_settings, PMM_Ψ, PMM_leader, PMM_follower, PMM_test, TP_ranges
+export PMM_settings, PMM_Ψ, PMM_leader, PMM_follower, PMM_test, PMM_ranges, TP_ranges
 export SMD_Ψ
 
 include("TP.jl")
